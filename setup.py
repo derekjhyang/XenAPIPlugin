edccd47e4f2dbff270060ec589c26c7ad117835c
SAMEVED <NCU.PDCLAB.SAMEVED@gmail.com>
2013-05-31 13:36:58 +0800

    add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name="XenAPIPLugin",
+    url="https://github.com/xen-org/xen-api/blob/master/scripts/examples/python/XenAPIPlugin.py",
+    package=['XenAPIPlugin'],
+ 
+)
