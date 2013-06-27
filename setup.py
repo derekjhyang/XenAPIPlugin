17cb2a9326b9f904d515a661c13e38ecd0c7de17
hswayne77 <hswayne77@gmail.com>
2013-06-28 04:09:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-
 import setuptools
-
 setuptools.setup(
     name="XenAPIPLugin",
+    version="1.3.2",
     url="https://github.com/xen-org/xen-api/blob/master/scripts/examples/python/XenAPIPlugin.py",
     package=['XenAPIPlugin'],
- 
 )
