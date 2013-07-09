11b6a481ae627edfc50c5135cbe8d67d04d998f7
SAMEVED <NCU.PDCLAB.SAMEVED@gmail.com>
2013-07-10 04:21:07 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,4 @@ setuptools.setup(
     name="XenAPIPlugin",
     version="1.3.2",
     url="https://github.com/xen-org/xen-api/blob/master/scripts/examples/python/XenAPIPlugin.py",
-    package=['XenAPIPlugin'],
 )
