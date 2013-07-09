28e8787270dca8b4fd3841cb61ef49d0aee90c61
hswayne77 <hswayne77@gmail.com>
2013-07-10 00:26:10 +0800

    fix package name typo error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import setuptools
 setuptools.setup(
-    name="XenAPIPLugin",
+    name="XenAPIPlugin",
     version="1.3.2",
     url="https://github.com/xen-org/xen-api/blob/master/scripts/examples/python/XenAPIPlugin.py",
     package=['XenAPIPlugin'],
